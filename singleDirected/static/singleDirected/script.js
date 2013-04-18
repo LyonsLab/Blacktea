02b3b1003e8c78272067ee7093c4274195f12c25
Roey Chasman <rchasman@gmail.com>
2013-04-18 09:10:42 -0700

    Bug fixes.

--- singleDirected/static/singleDirected/script.js
+++ singleDirected/static/singleDirected/script.js
@@ -1,10 +1,10 @@
-$(function() {
-    var w = Math.max(400, $(window).width()); // Width global
-    var h = Math.max(800, $(window).height()); // Height global
+    var w = Math.max(800, $(window).width()); // Width global
+    var h = Math.max(450, $(window).height()); // Height global
     var node, link, root; // Force Directed Graph globals
     var color = d3.scale.category20(); // Color Scale
     var hidden = new Object(); // Object that holds hidden nodes
 
+$(function() {
     // Setup D3
     force = d3.layout.force()
         .on("tick", tick)
@@ -28,201 +28,201 @@ $(function() {
         update();
         drawLegend();
     });
-
-    function drawLegend() {
-        var legend = d3.select("#legend")
-            .append("svg:svg")
-            .attr("width", w/4)
-            .attr("height", h * .6);
-
-        legend.selectAll("rect")
-            .data(data)
-            .enter()
-            .append("svg:rect")
-            .attr("x", 35)
-            .attr("y", function(d, i){ return (i *  20) + 50;})
-            .attr("width", 10)
-            .attr("height", 10)
-            .style("fill", fill)
-            .on("mousedown", hide);
-
-        legend.selectAll('text')
-            .data(data)
-            .enter()
-            .append("text")
-            .attr("x", 49)
-            .attr("y", function(d, i){ return (i *  20) + 59;})
-            .text(function(d){ if(d.type != "User") return d.name;})
-            .on("mousedown", hide);
-    };
-
-    function update() {
-        var nodes = flatten(root),
-        links = d3.layout.tree().links(nodes);
-
-        // Restart the force layout.
-        force
-            .nodes(nodes)
-            .links(links)
-            .start();
-
-        // Update the links…
-        link = vis.selectAll("line.link")
-            .data(links, function(d) { return d.target.id; });
-
-        // Enter any new links.
-        link.enter()
-            .insert("svg:line", ".node")
-            .attr("class", "link")
-            .attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
-
-        // Exit any old links.
-        link.exit().remove();
-
-        // Update the nodes…
-        node = vis.selectAll("circle.node")
-            .data(nodes, function(d) { return d.id; });
-
-        // Enter any new nodes.
-        var tooltip = d3.select("body")
-            .append("div")
-            .attr("id", "tooltip");
-
-        node.enter()
-            .append("svg:circle")
-            .attr("class", "node")
-            .attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; })
-            .attr("r", function(d) {
-                if (d.type == "Job") size = 4;
-                if (d.type == "User") size = 15;
-                else size = Math.sqrt(d.size) || 5;
-                if (size < 5) size = 8;
-                return size;
-            })
-            .style("fill", fill)
-            .on("click", click)
-            .on("mouseover", function(d) {
-                if (d.type != 'Job') {
-                    return tooltip.style("visibility", "visible")
-                            .text(d.name + " : " + d.size);
-                } else {
-                    return tooltip.style("visibility", "visible")
-                            .text("date: "  + d.date + "\n\nLink: " + d.link + "\n\nlog id: " + d.log_id);
-                }
-            })
-            .on("mousemove", function(){
-                return tooltip
-                    .style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
-            })
-            .on("mouseout", function(){
-                return tooltip
-                    .style("visibility", "hidden");
-            })
-            .on("mousedown", function(d){
-                if (d.type != 'Job' && d.size > 0)
-                    d.fixed = true;
-            })
-            .call(force.drag);
-
-        // Exit any old nodes.
-        node.exit().remove();
-    }
-
-    function tick() {
-        link.attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
-
-        node.attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; })
-    }
-
-    // Toggle nodes on legend click
-    function hide(node) {
-        if (hidden[node.name]) {
-            root.children.splice(node.id - 1, 0, hidden[node.name]);
-            delete hidden[node.name];
-        } else {
-            hidden[node.name] = root.children.splice(node.id - 1, 1)[0];
-        }
-        update();
-    }
-
-    // Color nodes
-    function fill(node) {
-        if (node.type == "Job") {
-            if (node.link == null) {
-                return 'Red';
-            } else {
-                return 'Green';
-            }
-        }
-        if (node.size > 0) {
-            if (node.type == 'Type') {
-                return color(node.name);
-            } else if (node.type == 'User') {
-                return 'white';
-            }
-            return 'white';
-        } else {
-            return 'grey';
-        }
-    }
-
-    // Toggle children on click.
-    function click(node) {
-        if (node.type == "Job") {
-            if(node.link != null) {
-                url = "http://genomevolution.org"
-                if(new RegExp(url).test(node.link)){
-                    window.open(node.link);
-                } else {
-                    window.open(url + "/CoGe/" + node.link);
-                }
-            }
-        } else if(node.type == "User" || node.size > 0) {
-            if (_.isEmpty(node.children) && (!node._children)) {
-                if(node.type == 'User'){
-                    url = "root/" + node.user_id
-                } else if (node.type == 'Type') {
-                    url = "user/" + node.user_id + "/type/"+ node.name
-                }
-                d3.json(BASE_URL + url, function(json) {
-                    node.children = json;
-                    update();
-                });
-                node._children = null;
-            } else if (!_.isNull(node.children)) {
-                node._children = node.children;
-                node.children = null;
-                node.fixed = false;
-                update();
-            } else {
-                node.children = node._children;
-                update();
-            }
-        }
-    }
-
-
-    // Returns a list of all nodes under the root.
-    var i = 0;
-    function flatten(root) {
-        var nodes = [];
-
-        function recurse(node) {
-            if (node.children) node.children.forEach(recurse);
-            if (!node.id) node.id = ++i;
-            if (node.type == 'User') node.fixed = true;
-            nodes.push(node);
-        }
-
-        recurse(root);
-        return nodes;
-    }
 });
+
+function drawLegend() {
+    var legend = d3.select("#legend")
+        .append("svg:svg")
+        .attr("width", w * .4)
+        .attr("height", h * .5);
+
+    legend.selectAll("rect")
+        .data(data)
+        .enter()
+        .append("svg:rect")
+        .attr("x", 35)
+        .attr("y", function(d, i){ return (i *  20) + 50;})
+        .attr("width", 10)
+        .attr("height", 10)
+        .style("fill", fill)
+        .on("mousedown", hide);
+
+    legend.selectAll('text')
+        .data(data)
+        .enter()
+        .append("text")
+        .attr("x", 49)
+        .attr("y", function(d, i){ return (i *  20) + 59;})
+        .text(function(d){ if(d.type != "User") return d.name;})
+        .on("mousedown", hide);
+};
+
+function update() {
+    var nodes = flatten(root),
+    links = d3.layout.tree().links(nodes);
+
+    // Restart the force layout.
+    force
+        .nodes(nodes)
+        .links(links)
+        .start();
+
+    // Update the links…
+    link = vis.selectAll("line.link")
+        .data(links, function(d) { return d.target.id; });
+
+    // Enter any new links.
+    link.enter()
+        .insert("svg:line", ".node")
+        .attr("class", "link")
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    // Exit any old links.
+    link.exit().remove();
+
+    // Update the nodes…
+    node = vis.selectAll("circle.node")
+        .data(nodes, function(d) { return d.id; });
+
+    // Enter any new nodes.
+    var tooltip = d3.select("body")
+        .append("div")
+        .attr("id", "tooltip");
+
+    node.enter()
+        .append("svg:circle")
+        .attr("class", "node")
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; })
+        .attr("r", function(d) {
+            if (d.type == "Job") size = 4;
+            if (d.type == "User") size = 15;
+            else size = Math.sqrt(d.size) || 5;
+            if (size < 5) size = 8;
+            return size;
+        })
+        .style("fill", fill)
+        .on("click", click)
+        .on("mouseover", function(d) {
+            if (d.type != 'Job') {
+                return tooltip.style("visibility", "visible")
+                        .text(d.name + " : " + d.size);
+            } else {
+                return tooltip.style("visibility", "visible")
+                        .text("date: "  + d.date + "\n\nLink: " + d.link + "\n\nlog id: " + d.log_id);
+            }
+        })
+        .on("mousemove", function(){
+            return tooltip
+                .style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
+        })
+        .on("mouseout", function(){
+            return tooltip
+                .style("visibility", "hidden");
+        })
+        .on("mousedown", function(d){
+            if (d.type != 'Job' && d.size > 0)
+                d.fixed = true;
+        })
+        .call(force.drag);
+
+    // Exit any old nodes.
+    node.exit().remove();
+}
+
+function tick() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; })
+}
+
+// Toggle nodes on legend click
+function hide(node) {
+    if (hidden[node.name]) {
+        root.children.splice(node.id - 1, 0, hidden[node.name]);
+        delete hidden[node.name];
+    } else {
+        hidden[node.name] = root.children.splice(node.id - 1, 1)[0];
+    }
+    update();
+}
+
+// Color nodes
+function fill(node) {
+    if (node.type == "Job") {
+        if (node.link == null) {
+            return 'Red';
+        } else {
+            return 'Green';
+        }
+    }
+    if (node.size > 0) {
+        if (node.type == 'Type') {
+            return color(node.name);
+        } else if (node.type == 'User') {
+            return 'white';
+        }
+        return 'white';
+    } else {
+        return 'grey';
+    }
+}
+
+// Toggle children on click.
+function click(node) {
+    if (node.type == "Job") {
+        if(node.link != null) {
+            url = "http://genomevolution.org"
+            if(new RegExp(url).test(node.link)){
+                window.open(node.link);
+            } else {
+                window.open(url + "/CoGe/" + node.link);
+            }
+        }
+    } else if(node.type == "User" || node.size > 0) {
+        if (_.isEmpty(node.children) && (!node._children)) {
+            if(node.type == 'User'){
+                url = "root/" + node.user_id
+            } else if (node.type == 'Type') {
+                url = "user/" + node.user_id + "/type/"+ node.name
+            }
+            d3.json(BASE_URL + url, function(json) {
+                node.children = json;
+                update();
+            });
+            node._children = null;
+        } else if (!_.isNull(node.children)) {
+            node._children = node.children;
+            node.children = null;
+            node.fixed = false;
+            update();
+        } else {
+            node.children = node._children;
+            update();
+        }
+    }
+}
+
+
+// Returns a list of all nodes under the root.
+var i = 0;
+function flatten(root) {
+    var nodes = [];
+
+    function recurse(node) {
+        if (node.children) node.children.forEach(recurse);
+        if (!node.id) node.id = ++i;
+        if (node.type == 'User') node.fixed = true;
+        nodes.push(node);
+    }
+
+    recurse(root);
+    return nodes;
+}
--- singleDirected/urls.py
+++ singleDirected/urls.py
@@ -3,7 +3,7 @@ from singleDirected.views import index, user, job, list
 
 urlpatterns = patterns(
     '',
-    url(r'^\d/', index, name='index'),
+    url(r'^\d+/', index, name='index'),
     url(r'^root/(?P<user_id>\d+)/$', user, name='user'),
     url(r'^root/(?P<user_id>\d+)/(?P<list>(?:[a-zA-Z\._]*/)*)', list, name='list'),
     url(r'^user/(?P<user_id>\d+)/type/(?P<job>[a-zA-Z\._]+)$', job, name='job'),
